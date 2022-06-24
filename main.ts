219e702c664dff9b2fd2b23f6222939ee905570d
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 17:06:44 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+function novo_desafio () {
+	
+}
+input.onButtonPressed(Button.A, function () {
+	
+})
+function verificar_resultado () {
+	
+}
+function iniciar_movimento () {
+	
+}
+function contagem_regressiva () {
+	
+}
+function solucao_desafio () {
+	
+}
+input.onButtonPressed(Button.B, function () {
+	
+})
+function executar_funcoes () {
+    novo_desafio()
+    contagem_regressiva()
+    iniciar_movimento()
+    verificar_resultado()
+}
+let iniciado = false
+let velocidade = 500
+game.setScore(0)
+basic.showArrow(ArrowNames.West)
